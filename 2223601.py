eef4fb8e314bda3a34a91d8859ede0ff84ba8234
Anisha4020 <anishaghadge45@gmail.com>
2025-03-27 16:31:09 +0530

    Create 2223601.py

---
+++ 2223601.py
@@ -0,0 +1,9 @@
+def run() :     
+    # import modules here 
+    import pandas as pd  # type: ignore
+    import numpy as np # type: ignore
+
+    # logic 
+    df = pd.read_csv(r"C:\Users\S42104\Desktop\bajaj\Data Engineering\data - sample.xlsx")
+    print(df.head())
+    return df
